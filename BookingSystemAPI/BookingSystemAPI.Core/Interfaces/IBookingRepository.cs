c879c7b562a268ba4d48efe6bd1fa3d6a742b757
Daniel Cornelius <danielwaynecornelius@gmail.com>
2026-03-17 14:06:28 +0200

    repository interface

---
+++ BookingSystemAPI/BookingSystemAPI.Core/Interfaces/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookingSystemAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystemAPI.Core.Interfaces
+{
+    public interface IBookingRepository
+    {
+        Task<BookingModel> AddBookingAsync(CreateBookingRequestModel createBooking);
+        Task<BookingModel> UpdateBookingAsync(UpdateBookingRequestModel updateBooking);
+        Task<BookingModel> DeleteBookingAsync(DeleteBookingRequestModel deleteBooking);
+    }
+}
