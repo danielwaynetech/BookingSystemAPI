e591b354bec4ed652c37460d317cec3d809a03b2
Daniel Cornelius <danielwaynecornelius@gmail.com>
2026-03-17 13:49:11 +0200

    add booking model

---
+++ BookingSystemAPI/BookingSystemAPI.Core/Interfaces/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystemAPI.Core.Interfaces
+{
+    public interface IBookingService
+    {
+    }
+}
--- BookingSystemAPI/BookingSystemAPI.Core/Models/BookingModel.cs
+++ BookingSystemAPI/BookingSystemAPI.Core/Models/BookingModel.cs
@@ -7,14 +7,16 @@ namespace BookingSystemAPI.Core.Models
     public class BookingModel
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public string BookTitle { get; set; }
-        public string Author { get; set; }
+        // User Email as Id of the person making the booking
+        public string UserEmail { get; set; } = string.Empty;
+        public string BookTitle { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
         public DateTime BookingDate { get; set; }
         public DateTime HolidayStartDate { get; set; }
         public DateTime HolidayEndDate { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
