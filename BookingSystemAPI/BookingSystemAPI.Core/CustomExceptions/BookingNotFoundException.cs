8ff9709f04ab2f67f315db477e76e3a12ff842a2
Daniel Cornelius <danielwaynecornelius@gmail.com>
2026-03-17 14:28:48 +0200

    custom exceptions

---
+++ BookingSystemAPI/BookingSystemAPI.Core/CustomExceptions/BookingNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystemAPI.Core.CustomExceptions
+{
+    public class BookingNotFoundException : Exception
+    {
+        public BookingNotFoundException(Guid bookingId)
+            : base($"Booking with ID '{bookingId}' was not found.")
+        {
+        }
+    }
+}
---
+++ BookingSystemAPI/BookingSystemAPI.Core/CustomExceptions/BookingValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystemAPI.Core.CustomExceptions
+{
+    public class BookingValidationException : Exception
+    {
+        public ValidationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
