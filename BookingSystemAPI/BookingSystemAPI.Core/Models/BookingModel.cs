0ba58f0a720ad7adf7976a4d38430c7db6718c3b
Daniel Cornelius <danielwaynecornelius@gmail.com>
2026-03-17 13:42:04 +0200

    add core

---
+++ BookingSystemAPI/BookingSystemAPI.Core/Models/BookingModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingSystemAPI.Core.Models
+{
+    public class BookingModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime HolidayStartDate { get; set; }
+        public DateTime HolidayEndDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
